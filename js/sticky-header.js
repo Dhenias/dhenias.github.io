be3be2c64a0254b86d16bbc61b6c293e1ee3b8c2
Dhenias <dhenias08@gmail.com>
2025-05-26 22:02:39 +0700

    update 5 files

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,12 +1,16 @@
 window.onscroll = function() {stickyHeader()};
 
 var header = document.getElementById("header");
-var sticky = header.offsetTop - 170;
+var sticky = header.offsetTop;
+var body = document.querySelector('.body');
+var headerHeight = header.offsetHeight;
 
 function stickyHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    if (body) body.style.paddingTop = headerHeight + "px";
   } else {
     header.classList.remove("sticky");
+    if (body) body.style.paddingTop = "";
   }
 }
